d14186d58f774d6cafabce011c25daa96d74dbf3
Benjamin Eder <barny.eder@gmail.com>
2018-04-09 19:35:35 +0200

    Added favicon

--- web/main.dart
+++ web/main.dart
@@ -4,17 +4,11 @@ import 'package:netzwerke_animationen/app_component.dart';
 import 'package:netzwerke_animationen/src/services/i18n_service/i18n_service.dart';
 
 void main() {
-  bool isDev = false;
-
   List customProviders = [
     ROUTER_PROVIDERS,
-    I18nService
+    I18nService,
+    provide(LocationStrategy, useClass: HashLocationStrategy)
   ];
-
-  // Add development providers, which will get removed when in production.
-  if (isDev) {
-    customProviders.add(provide(LocationStrategy, useClass: HashLocationStrategy));
-  }
 
   bootstrap(AppComponent, customProviders);
 }
