6ada44023316bc8a43a63a95dc9f783828ef8559
Benjamin Eder <barny.eder@gmail.com>
2018-04-21 11:21:02 +0200

    Added start for send and wait animation

--- lib/src/services/animation_service/animation_service.dart
+++ lib/src/services/animation_service/animation_service.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:angular/angular.dart';
 import 'package:netzwerke_animationen/src/ui/animations/animation_descriptor.dart';
+import 'package:netzwerke_animationen/src/ui/animations/reliable_transmission/send_and_wait/send_and_wait_animation.dart';
 import 'package:netzwerke_animationen/src/ui/animations/transmission/transmission_animation.dart';
 
 /**
@@ -11,6 +12,7 @@ class AnimationService {
 
   Future<Map<String, AnimationDescriptor>> getAnimationDescriptors() async => {
     TransmissionAnimation.DESCRIPTOR.path: TransmissionAnimation.DESCRIPTOR,
+    SendAndWaitAnimation.DESCRIPTOR.path: SendAndWaitAnimation.DESCRIPTOR
   };
 
 }
---
+++ lib/src/ui/animations/reliable_transmission/send_and_wait/send_and_wait_animation.dart
@@ -0,0 +1,45 @@
+import 'dart:html';
+import 'dart:math';
+import "package:angular/angular.dart";
+import 'package:angular_components/angular_components.dart';
+import 'package:netzwerke_animationen/src/services/i18n_service/i18n_pipe.dart';
+import 'package:netzwerke_animationen/src/services/i18n_service/i18n_service.dart';
+import 'package:netzwerke_animationen/src/ui/animations/animation_descriptor.dart';
+import 'package:netzwerke_animationen/src/ui/canvas/animation/canvas_animation.dart';
+import 'package:netzwerke_animationen/src/ui/canvas/canvas_component.dart';
+
+@Component(
+  selector: "send-and-wait-animation",
+  templateUrl: "send_and_wait_animation.html",
+  styleUrls: const ["send_and_wait_animation.css"],
+  directives: const [CORE_DIRECTIVES, materialDirectives, CanvasComponent],
+  pipes: const [I18nPipe]
+)
+class SendAndWaitAnimation extends CanvasAnimation implements OnInit {
+
+  /**
+   * Descriptor for the animation.
+   */
+  static const AnimationDescriptor DESCRIPTOR = const AnimationDescriptor(SendAndWaitAnimation, "send-and-wait-animation.name", "img/image-preview.svg", "send-and-wait");
+
+  I18nService _i18n;
+
+  SendAndWaitAnimation(this._i18n);
+
+  @override
+  ngOnInit() {
+
+  }
+
+  @override
+  void render(num timestamp) {
+    context.clearRect(0, 0, size.width, size.height);
+
+    context.textBaseline = "center";
+    context.font = "5.0em 'Roboto'";
+    context.textAlign = "center";
+
+    context.fillText("Yet to be implemented.", size.width / 2, size.height / 2);
+  }
+
+}
