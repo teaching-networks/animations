7ba03ab4e4df88d2ce23e5d016f1122c67b06d1c
Benjamin Eder <barny.eder@gmail.com>
2018-09-18 20:02:41 +0200

    Further progress in the user management component

--- lib/src/ui/view/user-management/user_management_component.dart
+++ lib/src/ui/view/user-management/user_management_component.dart
@@ -1,4 +1,6 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/material_button/material_button.dart';
+import 'package:angular_components/material_icon/material_icon.dart';
 import 'package:angular_components/material_spinner/material_spinner.dart';
 import 'package:angular_components/material_toggle/material_toggle.dart';
 import 'package:angular_router/angular_router.dart';
@@ -7,12 +9,19 @@ import 'package:hm_animations/src/services/user_service/model/user.dart';
 import 'package:hm_animations/src/services/user_service/user_service.dart';
 import 'package:hm_animations/src/ui/misc/directives/restricted_directive.dart';
 
-@Component(
-    selector: "user-management-component",
-    templateUrl: "user_management_component.html",
-    styleUrls: ["user_management_component.css"],
-    directives: [routerDirectives, coreDirectives, RestrictedDirective, MaterialToggleComponent, MaterialSpinnerComponent],
-    pipes: [I18nPipe])
+@Component(selector: "user-management-component", templateUrl: "user_management_component.html", styleUrls: [
+  "user_management_component.css"
+], directives: [
+  routerDirectives,
+  coreDirectives,
+  RestrictedDirective,
+  MaterialToggleComponent,
+  MaterialSpinnerComponent,
+  MaterialIconComponent,
+  MaterialButtonComponent
+], pipes: [
+  I18nPipe
+])
 class UserManagementComponent implements OnInit {
   final UserService _userService;
 
@@ -25,5 +34,17 @@ class UserManagementComponent implements OnInit {
     _userService.getUsers().then((newUsers) {
       users = newUsers;
     });
+  }
+
+  void createUser() {
+
+  }
+
+  void editUser(User user) {
+
+  }
+
+  void deleteUser(int id) async {
+    // await _userService.deleteUser(id);
   }
 }
