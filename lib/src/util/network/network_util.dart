2da058d3dc895d3344c4df12fa5ef514400a6eb8
Benjamin Eder <barny.eder@gmail.com>
2019-01-12 18:09:11 +0100

    Forgot to adjust the server URL

--- lib/src/util/network/network_util.dart
+++ lib/src/util/network/network_util.dart
@@ -1,7 +1,7 @@
 class NetworkUtil {
   /// URL of the server.
-//  static const String baseServerURL = "https://www.sam.cs.hm.edu:8443";
-  static const String baseServerURL = "http://localhost:4200";
+  static const String baseServerURL = "https://www.sam.cs.hm.edu:8443";
+//  static const String baseServerURL = "http://localhost:4200";
 
   /// Key of the JSON Web Token in local storage.
   static const String tokenKey = "token";
