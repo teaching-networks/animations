7e127ef83e1c4939eaab5d095c1fa8406e6e83a8
Benjamin Eder <barny.eder@gmail.com>
2019-09-16 06:13:39 +0200

    #36 | Prepared IP fragmentation animation

---
+++ lib/src/ui/animations/ip_fragmentation/ip_fragmentation_drawable.dart
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Munich University of Applied Sciences - https://hm.edu/
+ * Licensed under GNU General Public License 3 (See LICENSE.md in the repositories root)
+ */
+
+import 'package:hm_animations/src/ui/canvas/animation/v2/drawable.dart';
+
+/// Drawable for the IP fragmentation animation.
+class IPFragmentationDrawable extends Drawable {
+  @override
+  void draw() {
+    // TODO: implement draw
+  }
+
+  @override
+  bool needsRepaint() => false;
+
+  @override
+  void update(num timestamp) {
+    // Nothing to update
+  }
+}
